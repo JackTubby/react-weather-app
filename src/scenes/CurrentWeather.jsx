e16ca4ec21bea2c630c546739db0e3c4414abf35
JackTubby <jack200034@hotmail.com>
2022-11-06 23:00:55 +0000

    Create current weather layout

--- src/scenes/CurrentWeather.jsx
+++ src/scenes/CurrentWeather.jsx
@@ -1,6 +1,11 @@
 const CurrentWeather = () => {
     return (
-        <div>Current Weather</div>
+        <div>
+            <img/> {/* This will be where the sun/cloud/rain img will go (top right of screen) */}
+            <h2>23<span>&#176;</span></h2>
+            <h2>Cloudy</h2>
+            <h2>Paris, France</h2>
+        </div>
     )
 }
 
