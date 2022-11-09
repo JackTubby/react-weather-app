53c79b1610b9e4945a28df2c64ecdeb521c85d09
JackTubby <jack200034@hotmail.com>
2022-11-09 21:36:43 +0000

    Create tab conditional display functionality

--- src/scenes/Forcast.jsx
+++ src/scenes/Forcast.jsx
@@ -1,16 +1,46 @@
-const Forcast = () => {
-    return (
-        <div>
-            <div>
-                <h2>Today</h2>
-            </div>
-            <div>
-                <img/>
-                <p></p>
-                <p></p>
-            </div>
-        </div>
-    )
-}
+import React, { useState } from "react";
+import "./styles.css";
 
-export default Forcast
+import Today from './forcast/Today';
+import Tomorrow from './forcast/Tomorrow';
+import SevenDays from './forcast/SevenDays';
+
+const Forcast = () => {
+  // Create state to change tabs and return an index
+  const [activeIndex, setActiveIndex] = useState(1);
+  const handleClick = (index) => setActiveIndex(index);
+  const checkActive = (index, className) =>
+    activeIndex === index ? className : "";
+
+  return (
+    <div>
+      <div className="tabs">
+        <button
+          className={`tab ${checkActive(1, "active")}`}
+          onClick={() => {handleClick(1);}}
+        >
+          Today
+        </button>
+        <button
+          className={`tab ${checkActive(2, "active")}`}
+          onClick={() => {handleClick(2);}}
+        >
+          Tomorrow
+        </button>
+        <button
+          className={`tab ${checkActive(3, "active")}`}
+          onClick={() => {handleClick(3);}}
+        >
+          Seven Days
+        </button>
+        {
+          activeIndex === 1 ? <Today/> :
+          activeIndex === 2 ? <Tomorrow /> :
+          <SevenDays/>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Forcast;
