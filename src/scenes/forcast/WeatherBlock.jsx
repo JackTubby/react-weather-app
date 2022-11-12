8dfd4f944a8386a796c9cbb2eff48d2ba1cba4b1
JackTubby <jack200034@hotmail.com>
2022-11-12 20:25:50 +0000

    Changed i tags to p

--- src/scenes/forcast/WeatherBlock.jsx
+++ src/scenes/forcast/WeatherBlock.jsx
@@ -3,7 +3,7 @@ import "../styles.css";
 const WeatherBlock = () => {
     return (
         <div className="weather-block">
-            <i>Sun Img</i>
+            <p>Sun Img</p>
             <p>30&#176;</p>
             <p>12AM</p>
         </div>
