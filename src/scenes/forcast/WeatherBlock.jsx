648b4a54f8d94facde77d2cd4d5aa5adce01eb6e
JackTubby <jack200034@hotmail.com>
2022-11-12 20:07:25 +0000

    Create WeatherBlock & create layout

---
+++ src/scenes/forcast/WeatherBlock.jsx
@@ -0,0 +1,13 @@
+import "../styles.css";
+
+const WeatherBlock = () => {
+    return (
+        <div className="weather-block">
+            <i>Sun Img</i>
+            <p>30&#176;</p>
+            <p>12AM</p>
+        </div>
+    )
+}
+
+export default WeatherBlock
