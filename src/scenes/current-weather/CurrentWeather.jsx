6872339d1b05984ce27697a2783735af57c9af12
JackTubby <jack200034@hotmail.com>
2022-11-13 18:57:53 +0000

    Add default weather icon

--- src/scenes/current-weather/CurrentWeather.jsx
+++ src/scenes/current-weather/CurrentWeather.jsx
@@ -1,11 +1,12 @@
-import './currentWeather.css'
+import './currentWeather.css';
+import sunIcon from '../../images/sun-icon.svg';
 
 const CurrentWeather = () => {
     return (
         <div>
             {/* Icon top right of screen - vary between current weather */}
             <div className='current-icon-wrapper'>
-                <img />
+                <img className='current-icon' src={sunIcon} alt="Kiwi standing on oval"/>
             </div>
             <div className='current-info-wrapper'>
                 <h3 className='current-temp'>16&#176;</h3>
