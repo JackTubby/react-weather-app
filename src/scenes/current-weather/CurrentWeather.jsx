a5c7dc903cc7221174cf560a28a42a3f945adea1
JackTubby <jack200034@hotmail.com>
2022-11-13 17:03:19 +0000

    Add classNames

--- src/scenes/current-weather/CurrentWeather.jsx
+++ src/scenes/current-weather/CurrentWeather.jsx
@@ -4,13 +4,13 @@ const CurrentWeather = () => {
     return (
         <div>
             {/* Icon top right of screen - vary between current weather */}
-            <div>
+            <div className='current-icon-wrapper'>
                 <img />
             </div>
-            <div>
-                <h3>16&#176;</h3>
-                <h3>Cloudy</h3>
-                <h2>Paris, France</h2>
+            <div className='current-info-wrapper'>
+                <h3 className='current-temp'>16&#176;</h3>
+                <h3 className='current-sky'>Cloudy</h3>
+                <h2 className='current-location'>Paris, France</h2>
             </div>
         </div>
     )
