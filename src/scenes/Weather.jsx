93d5ec1dd0194213055a3cda141ba591b5c8a361
JackTubby <jack200034@hotmail.com>
2022-12-01 21:37:08 +0000

    Add form functionality to get user input

--- src/scenes/Weather.jsx
+++ src/scenes/Weather.jsx
@@ -1,15 +1,33 @@
 import CurrentWeather from './current-weather/CurrentWeather'
 import Forcast from './forcast/Forcast'
+import { useEffect, useState } from 'react'
 
 const Weather = () => {
+
+    const [formData, setFormData] = useState({
+        location: "",
+    })
+    function handleChange(event) {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+    console.log(formData)
+
     return (
         <div>
             <div>
                 <form>
                     <input
                         type="text"
+                        placeholder="Search Location"
+                        onChange={handleChange}
                         name="location"
-                        value="Search Location"
+                        value={formData.location}
                     />
                 </form>
             </div>
