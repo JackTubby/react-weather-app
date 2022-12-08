350ffd72808dba8a484e81e5c7aaf96f4db06e95
JackTubby <jack200034@hotmail.com>
2022-12-08 15:18:29 +0000

    Comment out props code whilst weather is under dev

--- src/scenes/current-weather/CurrentWeather.jsx
+++ src/scenes/current-weather/CurrentWeather.jsx
@@ -1,7 +1,14 @@
 import './currentWeather.css';
 import sunIcon from '../../images/sun-icon.svg';
 
-const CurrentWeather = () => {
+const CurrentWeather = (props) => {
+
+    // let temp;
+    // if (props.weather.main === undefined) {
+    //     console.log("yep")
+    // } else {
+    //     let temp = props ? `${Math.round(props.weather.main.temp)}°` : "16°"
+    // }
     return (
         <div>
             {/* Icon top right of screen - vary between current weather */}
@@ -9,7 +16,7 @@ const CurrentWeather = () => {
                 <img className='current-icon' src={sunIcon} alt="Kiwi standing on oval"/>
             </div>
             <div className='current-info-wrapper'>
-                <h3 className='current-temp'>16&#176;</h3>
+                {/* <h3 className='current-temp'>{`${Math.round(props.weather.main.temp)}°`}</h3> */}
                 <h3 className='current-sky'>Cloudy</h3>
                 <h2 className='current-location'>Paris, France</h2>
             </div>
