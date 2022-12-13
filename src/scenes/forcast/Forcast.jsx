54571c366765868d4056ae861c4a82d387b377dd
Jack <jack200034@hotmail.com>
2022-12-13 22:23:06 +0000

    Created getCoord function & formatted file

--- src/scenes/forcast/Forcast.jsx
+++ src/scenes/forcast/Forcast.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./forcast.css";
 
-import Today from './Today';
-import Tomorrow from './Tomorrow';
-import SevenDays from './SevenDays';
+import Today from "./Today";
+import Tomorrow from "./Tomorrow";
+import SevenDays from "./SevenDays";
 
 const Forcast = () => {
   // Create state to change tabs and return an index
@@ -12,34 +12,60 @@ const Forcast = () => {
   const checkActive = (index, className) =>
     activeIndex === index ? className : "";
 
+  // COORDS
+  const [coords, setCoords] = useState();
+  useEffect(() => {
+    async function getCoords() {
+    const key = "";
+    const location = "london";
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${key}`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+      }
+      const coordsResponse = await response.json();
+      setCoords(coordsResponse);
+    };
+    getCoords();
+  }, []);
+
   return (
     <div className="tabs-wrapper">
       <div className="tabs">
         <button
           className={`tab ${checkActive(1, "active")}`}
-          onClick={() => {handleClick(1);}}
+          onClick={() => {
+            handleClick(1);
+          }}
         >
           Today
         </button>
         <button
           className={`tab ${checkActive(2, "active")}`}
-          onClick={() => {handleClick(2);}}
+          onClick={() => {
+            handleClick(2);
+          }}
         >
           Tomorrow
         </button>
         <button
           className={`tab ${checkActive(3, "active")}`}
-          onClick={() => {handleClick(3);}}
+          onClick={() => {
+            handleClick(3);
+          }}
         >
           Next 7 Days
         </button>
       </div>
       <div className="forcast-wrapper">
-      {
-          activeIndex === 1 ? <Today/> :
-          activeIndex === 2 ? <Tomorrow /> :
-          <SevenDays/>
-        }
+        {activeIndex === 1 ? (
+          <Today />
+        ) : activeIndex === 2 ? (
+          <Tomorrow />
+        ) : (
+          <SevenDays />
+        )}
       </div>
     </div>
   );
