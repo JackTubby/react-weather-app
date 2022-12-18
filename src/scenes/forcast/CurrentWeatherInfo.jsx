9602de5cbf81500c8dcd69290b050adc929c6bba
Jack <jack200034@hotmail.com>
2022-12-18 21:53:40 +0000

    Change classNames & fix pressure to mb

--- src/scenes/forcast/CurrentWeatherInfo.jsx
+++ src/scenes/forcast/CurrentWeatherInfo.jsx
@@ -2,56 +2,71 @@ import "./forcast.css";
 import { BsFillSunFill } from "react-icons/bs";
 
 const CurrentWeatherInfo = (props) => {
-    return (
-                    <div className="today--tab">
-            <div className="today--card-wrapper">
-                <div className="card--icon"><BsFillSunFill /></div>
-                <div className="card--info-wrapper">
-                    <div className="info">
-                        <p>{props.today.feels_like}&#176;</p>
-                        <p>Feels Like</p>
-                    </div>
-                </div>
-            </div>
-            <div className="today--card-wrapper">
-                <div className="card--icon"><BsFillSunFill /></div>
-                <div className="card--info-wrapper">
-                    <div className="info">
-                        <p>{props.today.wind} mph</p>
-                        <p>Wind</p>
-                    </div>
-                </div>
-            </div>
-            <div className="today--card-wrapper">
-                <div className="card--icon"><BsFillSunFill /></div>
-                <div className="card--info-wrapper">
-                    <div className="info">
-                        <p>{props.today.humidity}%</p>
-                        <p>Humidity</p>
-                    </div>
-                </div>
-            </div>
-            <div className="today--card-wrapper">
-                <div className="card--icon"><BsFillSunFill /></div>
-                <div className="card--info-wrapper">
-                    <div className="info">
-                        <p>{props.today.pressure}%</p>
-                        <p>Pressure</p>
-                    </div>
-                </div>
-            </div>
-            <div className="today--card-wrapper">
-                <div className="card--icon"><BsFillSunFill /></div>
-                <div className="card--info-wrapper">
-                    <div className="info">
-                        <p>{props.today.cloud_type}</p>
-                        <p>Cloud Type</p>
-                    </div>
-                </div>
-            </div>
-
+  return (
+    <div className="info--tab">
+      <div className="info--card-wrapper">
+        <div className="card--icon">
+          <BsFillSunFill />
         </div>
-    )
-}
+        <div className="card--info-wrapper">
+          <div className="info">
+            <p>{Math.round(props.today.feels_like)}&#176;</p>
+            <p>Feels Like</p>
+          </div>
+        </div>
+      </div>
+      <div className="info--card-wrapper">
+        <div className="card--icon">
+          <BsFillSunFill />
+        </div>
+        <div className="card--info-wrapper">
+          <div className="info">
+            <p>
+              {props.today.wind}
+              <i>mph</i>
+            </p>
+            <p>Wind</p>
+          </div>
+        </div>
+      </div>
+      <div className="info--card-wrapper">
+        <div className="card--icon">
+          <BsFillSunFill />
+        </div>
+        <div className="card--info-wrapper">
+          <div className="info">
+            <p>{props.today.humidity}%</p>
+            <p>Humidity</p>
+          </div>
+        </div>
+      </div>
+      <div className="info--card-wrapper">
+        <div className="card--icon">
+          <BsFillSunFill />
+        </div>
+        <div className="card--info-wrapper">
+          <div className="info">
+            <p>
+              {props.today.pressure}
+              <i>mb</i>
+            </p>
+            <p>Pressure</p>
+          </div>
+        </div>
+      </div>
+      <div className="info--card-wrapper">
+        <div className="card--icon">
+          <BsFillSunFill />
+        </div>
+        <div className="card--info-wrapper">
+          <div className="info">
+            <p>{props.today.cloud_type}</p>
+            <p>Cloud Type</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CurrentWeatherInfo
+export default CurrentWeatherInfo;
