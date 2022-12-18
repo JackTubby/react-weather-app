7f2508ef96cef6592d6a58595558dd6b22c60955
Jack <jack200034@hotmail.com>
2022-12-18 21:39:07 +0000

    Moved today content into current info

--- src/scenes/forcast/CurrentWeatherInfo.jsx
+++ src/scenes/forcast/CurrentWeatherInfo.jsx
@@ -1,8 +1,54 @@
 import "./forcast.css";
+import { BsFillSunFill } from "react-icons/bs";
 
-const CurrentWeatherInfo = () => {
+const CurrentWeatherInfo = (props) => {
     return (
-        <div>
+                    <div className="today--tab">
+            <div className="today--card-wrapper">
+                <div className="card--icon"><BsFillSunFill /></div>
+                <div className="card--info-wrapper">
+                    <div className="info">
+                        <p>{props.today.feels_like}&#176;</p>
+                        <p>Feels Like</p>
+                    </div>
+                </div>
+            </div>
+            <div className="today--card-wrapper">
+                <div className="card--icon"><BsFillSunFill /></div>
+                <div className="card--info-wrapper">
+                    <div className="info">
+                        <p>{props.today.wind} mph</p>
+                        <p>Wind</p>
+                    </div>
+                </div>
+            </div>
+            <div className="today--card-wrapper">
+                <div className="card--icon"><BsFillSunFill /></div>
+                <div className="card--info-wrapper">
+                    <div className="info">
+                        <p>{props.today.humidity}%</p>
+                        <p>Humidity</p>
+                    </div>
+                </div>
+            </div>
+            <div className="today--card-wrapper">
+                <div className="card--icon"><BsFillSunFill /></div>
+                <div className="card--info-wrapper">
+                    <div className="info">
+                        <p>{props.today.pressure}%</p>
+                        <p>Pressure</p>
+                    </div>
+                </div>
+            </div>
+            <div className="today--card-wrapper">
+                <div className="card--icon"><BsFillSunFill /></div>
+                <div className="card--info-wrapper">
+                    <div className="info">
+                        <p>{props.today.cloud_type}</p>
+                        <p>Cloud Type</p>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
