792650ad625ca80fa21c8bf47d4a398b2138fe61
JackTubby <jack200034@hotmail.com>
2022-12-22 21:10:36 +0000

    Add props to component

--- src/scenes/forcast/WeatherBlock.jsx
+++ src/scenes/forcast/WeatherBlock.jsx
@@ -1,12 +1,19 @@
 import "./forcast.css";
 import { BsFillSunFill } from "react-icons/bs";
 
-const WeatherBlock = () => {
+const WeatherBlock = (props) => {
+
+    const get_dt = props.dt_txt
+    const split_dt = get_dt.split(" ")
+    const get_time = split_dt[1]
+    const time = get_time.slice(0, 5)
+    
+
     return (
-        <div className="weather-block">
-            <p><BsFillSunFill /></p>
-            <p>30&#176;</p>
-            <p>12AM</p>
+        <div className="weather--block">
+            <p>{Math.round(props.main.temp)}&#176;</p>
+            <p><BsFillSunFill className="icon" /></p>
+            <p>{time}</p>
         </div>
     )
 }
