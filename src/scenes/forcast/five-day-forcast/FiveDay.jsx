82e4aaf36c3948792937c434c96c9af3c1b9842c
JackTubby <jack200034@hotmail.com>
2022-12-22 21:42:21 +0000

    Get date from forecast & pass as prop

--- src/scenes/forcast/five-day-forcast/FiveDay.jsx
+++ src/scenes/forcast/five-day-forcast/FiveDay.jsx
@@ -2,10 +2,20 @@ import "../forcast.css";
 import "./fiveday.css"
 import WeatherBlock from "../WeatherBlock";
 
-const FiveDay = () => {
+const FiveDay = (props) => {
+    const forcasts = props.fiveDays.map(forcast => {
+        const get_dt = forcast.dt_txt
+        const split_dt = get_dt.split(" ")
+        const get_date = split_dt[0]
+        const date = get_date.slice(8)
+        console.log(date)
+        return (
+            <WeatherBlock {...forcast} d_or_t={date} />
+            )
+        })
     return (
         <div className="forcast-tab">
-            <WeatherBlock />
+            {forcasts}
         </div>
     )
 }
