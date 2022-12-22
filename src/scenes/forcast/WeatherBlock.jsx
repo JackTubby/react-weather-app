28dcd1e07a92850688c79f44a11ffcdd589225a8
JackTubby <jack200034@hotmail.com>
2022-12-22 21:42:39 +0000

    Remove date & time split and pass new prop

--- src/scenes/forcast/WeatherBlock.jsx
+++ src/scenes/forcast/WeatherBlock.jsx
@@ -3,17 +3,11 @@ import { BsFillSunFill } from "react-icons/bs";
 
 const WeatherBlock = (props) => {
 
-    const get_dt = props.dt_txt
-    const split_dt = get_dt.split(" ")
-    const get_time = split_dt[1]
-    const time = get_time.slice(0, 5)
-    
-
     return (
         <div className="weather--block">
             <p>{Math.round(props.main.temp)}&#176;</p>
             <p><BsFillSunFill className="icon" /></p>
-            <p>{time}</p>
+            <p>{props.d_or_t}</p>
         </div>
     )
 }
