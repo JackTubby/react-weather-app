b058f4fef7dac95aed7fa1de78b2eb406a2f2901
JackTubby <jack200034@hotmail.com>
2022-12-23 00:10:56 +0000

    Add identifier to forcast obj

--- src/scenes/forcast/five-day-forcast/FiveDay.jsx
+++ src/scenes/forcast/five-day-forcast/FiveDay.jsx
@@ -6,13 +6,14 @@ import WeatherBlock from "../WeatherBlock";
 const FiveDay = (props) => {
 
     const forcasts = props.fiveDays.map(forcast => {
+        let fiveDayObj = {...forcast, identifier: 'fiveDay'}
         // Get date
         const get_dt = forcast.dt_txt
         const split_dt = get_dt.split(" ")
         const get_date = split_dt[0]
         const day = get_date.slice(8)
         return (
-            <WeatherBlock {...forcast} d_or_t={day} />
+            <WeatherBlock {...fiveDayObj} d_or_t={day} />
             )
         })
     return (
--- src/scenes/forcast/tomorrow-forcast/Tomorrow.jsx
+++ src/scenes/forcast/tomorrow-forcast/Tomorrow.jsx
@@ -4,12 +4,13 @@ import WeatherBlock from "../WeatherBlock";
 
 const Tomorrow = (props) => {
     const forcasts = props.tomorrowForcast.map(forcast => {
+        let tomorrowObj = {...forcast, identifier: 'tomorrow'}
         const get_dt = forcast.dt_txt
         const split_dt = get_dt.split(" ")
         const get_time = split_dt[1]
         const time = get_time.slice(0, 5)
         return (
-            <WeatherBlock {...forcast} d_or_t={time} />
+            <WeatherBlock {...tomorrowObj} d_or_t={time} />
         )
     })
     return (
