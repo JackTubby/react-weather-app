5561d95b81b6cc6e46c2622304aa9815963f3b75
JackTubby <jack200034@hotmail.com>
2022-12-23 00:11:34 +0000

    Add check if date add identifier else show time

--- src/scenes/forcast/WeatherBlock.jsx
+++ src/scenes/forcast/WeatherBlock.jsx
@@ -1,13 +1,36 @@
+import React, { useState, useEffect } from "react";
+
 import "./forcast.css";
 import { BsFillSunFill } from "react-icons/bs";
 
 const WeatherBlock = (props) => {
 
+    const [checkIfDateOrTime, setCheckIfDateOrTime] = useState(props)
+    useEffect(function () {
+        if ( props.identifier == "fiveDay" ) {
+            const get_day = props.d_or_t
+            const second_char = props.d_or_t.charAt(1)
+            if (second_char == 1) {
+                setCheckIfDateOrTime({...props, d_or_t: `${get_day}st`})            
+            }
+            else if (second_char == 2) {
+                setCheckIfDateOrTime({...props, d_or_t: `${get_day}nd`})
+            }
+            else if (second_char == 3) {
+                setCheckIfDateOrTime({...props, d_or_t: `${get_day}rd`})
+            }
+            else {
+                setCheckIfDateOrTime({...props, d_or_t: `${get_day}th`})
+            }
+        }
+      }, []);
+    
+
     return (
         <div className="weather--block">
             <p>{Math.round(props.main.temp)}&#176;</p>
             <p><BsFillSunFill className="icon" /></p>
-            <p>{props.d_or_t}</p>
+            <p>{checkIfDateOrTime.d_or_t}</p>
         </div>
     )
 }
