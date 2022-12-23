74e53559b124f99ba867e7544f32fba5c27e3839
JackTubby <jack200034@hotmail.com>
2022-12-23 22:30:54 +0000

    Add today forecast passing data to weatherBlock

--- src/scenes/forcast/today-forcast/Today.jsx
+++ src/scenes/forcast/today-forcast/Today.jsx
@@ -1,11 +1,23 @@
 import "../forcast.css";
 import "./today.css"
+import WeatherBlock from "../weather-block/WeatherBlock";
 
-const Today = () => {
+const Today = (props) => {
+    const forcasts = props.todayForcast.map(forcast => {
+        let todayObj = {...forcast, identifier: 'today'}
+        // Get date
+        const get_dt = forcast.dt_txt
+        const split_dt = get_dt.split(" ")
+        const get_time = split_dt[1]
+        const time = get_time.slice(0, 5)
+        return (
+            <WeatherBlock {...todayObj} d_or_t={time} />
+            )
+        })
 
     return (
-        <div>
-            <h2>Today</h2>
+        <div className="forcast-tab">
+            {forcasts}
         </div>
     )
 }
