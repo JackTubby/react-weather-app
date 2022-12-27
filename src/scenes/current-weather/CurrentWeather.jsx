07c4f577c9c488468c7adedf8ff1ebc693cdfc89
JackTubby <jack200034@hotmail.com>
2022-12-27 22:52:10 +0000

    Updated page style to match designs

--- src/scenes/current-weather/CurrentWeather.jsx
+++ src/scenes/current-weather/CurrentWeather.jsx
@@ -1,30 +1,66 @@
-import './currentWeather.css';
-import sunIcon from '../../images/sun-icon.svg';
+import "./currentWeather.css";
+import { CiTempHigh } from "react-icons/ci";
 
 const CurrentWeather = (props) => {
-
-    const cloudData =  props.weather.clouds.all;
-    let clouds;
-    if(cloudData > 50){
-        clouds = "Cloudy"
-    } else if(cloudData > 10) {
-        clouds = "Partially Cloudy"
-    } else {
-        clouds = "Clear"
-    }
-    return (
-        <div>
-            {/* Icon top right of screen - vary between current weather */}
-            <div className='current-icon-wrapper'>
-                <img className='current-icon' src={sunIcon} alt="Kiwi standing on oval"/>
-            </div>
-            <div className='current-info-wrapper'>
-                <h3 className='current-temp'>{`${Math.round(props.weather.main.temp)}°`}</h3>
-                <h3 className='current-sky'>{clouds}</h3>
-                <h2 className='current-location'>{props.weather.name}</h2>
-            </div>
+  const cloudData = props.weather.clouds.all;
+  let clouds;
+  if (cloudData > 50) {
+    clouds = "Cloudy";
+  } else if (cloudData > 10) {
+    clouds = "Partially Cloudy";
+  } else {
+    clouds = "Clear";
+  }
+  return (
+    <div>
+      <div className="outer-wrapper">
+        <div className="header-wrapper">
+          <div className="location">London</div>
+          <div className="colour-mode">
+            <label class="toggle" for="myToggle">
+              <input
+                class="toggle-input"
+                name=""
+                type="checkbox"
+                id="myToggle"
+              />
+              <div class="toggle-fill"></div>
+            </label>
+          </div>
         </div>
-    )
-}
+        <div className="main-wrapper">
+          <div className="date">Monday, 12 June</div>
+          <div className="square">
+            <div className="weather-type">Thunderstorm</div>
+            <div className="temp">25</div>
+          </div>
+        </div>
+        <div className="extra-wrapper">
+          <div className="wind-wrapper">
+            <div className="extra-icon">
+              <CiTempHigh color="white" />
+            </div>
+            <div className="extra-text">9mph</div>
+            <div className="extra-heading">Wind</div>
+          </div>
+          <div className="humidity-wrapper">
+            <div className="extra-icon">
+              <CiTempHigh />
+            </div>
+            <div className="extra-text">30%</div>
+            <div className="extra-heading">Humdidity</div>
+          </div>
+          <div className="visibility-wrapper">
+            <div className="extra-icon">
+              <CiTempHigh />
+            </div>
+            <div className="extra-text">1.4km</div>
+            <div className="extra-heading">Visibility</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CurrentWeather
+export default CurrentWeather;
