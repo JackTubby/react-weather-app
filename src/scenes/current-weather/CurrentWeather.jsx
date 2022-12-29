cd308d9e830b96d7ac5493b59caed6a69ac29328
JackTubby <jack200034@hotmail.com>
2022-12-29 00:15:24 +0000

    Format document & add degree symbol

--- src/scenes/current-weather/CurrentWeather.jsx
+++ src/scenes/current-weather/CurrentWeather.jsx
@@ -5,7 +5,7 @@ import { WiHumidity } from "react-icons/wi";
 import { FaCloudscale } from "react-icons/fa";
 
 const CurrentWeather = (props) => {
-  console.log(props)
+  console.log(props);
   const cloudData = props.weather.clouds.all;
   let clouds;
   if (cloudData > 50) {
@@ -16,15 +16,15 @@ const CurrentWeather = (props) => {
     clouds = "Clear";
   }
 
-// Turn unix into organised todays date
+  // Turn unix into organised todays date
   const unix_timestamp = props.weather.dt;
   const date = new Date(unix_timestamp * 1000);
-  const date_to_string = date.toString()
-  const split_date = date_to_string.split(" ")
-  const get_day = split_date[0]
-  const get_date = split_date[2]
-  const get_month = split_date[1]
-  const today_date = `${get_day} ${get_date} ${get_month}`
+  const date_to_string = date.toString();
+  const split_date = date_to_string.split(" ");
+  const get_day = split_date[0];
+  const get_date = split_date[2];
+  const get_month = split_date[1];
+  const today_date = `${get_day} ${get_date} ${get_month}`;
 
   return (
     <div>
@@ -46,23 +46,29 @@ const CurrentWeather = (props) => {
         <div className="main-wrapper">
           <div className="date">{today_date}</div>
           <div className="square">
-            <div className="weather-type">{props.weather.weather[0].description}</div>
-            <div className="temp">{Math.round(props.weather.main.temp)}</div>
+            <div className="weather-type">
+              {props.weather.weather[0].description}
+            </div>
+            <div className="temp">{Math.round(props.weather.main.temp)}&#176;</div>
           </div>
         </div>
         <div className="extra-wrapper">
-        <div className="feels-wrapper">
+          <div className="feels-wrapper">
             <div className="extra-icon">
               <CiTempHigh />
             </div>
-            <div className="extra-text">{Math.round(props.weather.main.feels_like)}</div>
+            <div className="extra-text">
+              {Math.round(props.weather.main.feels_like)}&#176;
+            </div>
             <div className="extra-heading">Feels Like</div>
           </div>
           <div className="wind-wrapper">
             <div className="extra-icon">
               <BsWind />
             </div>
-            <div className="extra-text">{Math.round(props.weather.wind.speed)}mph</div>
+            <div className="extra-text">
+              {Math.round(props.weather.wind.speed)}mph
+            </div>
             <div className="extra-heading">Wind</div>
           </div>
           <div className="humidity-wrapper">
@@ -76,7 +82,9 @@ const CurrentWeather = (props) => {
             <div className="extra-icon">
               <BsEye />
             </div>
-            <div className="extra-text">{props.weather.visibility / 1000}km</div>
+            <div className="extra-text">
+              {props.weather.visibility / 1000}km
+            </div>
             <div className="extra-heading">Visibility</div>
           </div>
           <div className="pressure-wrapper">
